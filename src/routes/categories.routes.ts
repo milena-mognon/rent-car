bfb6cfc393447ad5c37eee6fb6f52edebd00c554
milena-mognon-biopark <77672152+milena-mognon-biopark@users.noreply.github.com>
2022-02-27 16:25:02 -0300

    Aula 6 - Criando Repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,18 @@
 import { Router } from 'express';
 
 import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoryRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoryRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoryRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.json(category);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
